058eba5499d453c1b74162e7875ee30dc89e5e34
Paulo Lobo <paulodamaso@gmail.com>
2017-11-24 11:56:28 -0200

    Repackaging

---
+++ src/java/main/MemoryTasks.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public final class MemoryTasks implements Tasks {
+    private final Iterable<Task> tasks;
+    
+    public MemoryTasks (Iterable<Task> tasks) {
+      this.tasks = new ArrayList<Task>((Collection<Task>)tasks);
+    }
+    
+    public Iterable<Task> iterate() {
+      return this.tasks;
+    }
+    
+    public Tasks add(Task task) {
+    	return new MemoryTasks(new ArrayList<Task>((Collection<Task>)tasks) {{add(task);}});
+    }
+}
